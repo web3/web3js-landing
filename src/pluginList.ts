d8c7aa54d6deef7e04e03920eeaeaebe61980a07
Sailesh Polavarapu <86889128+saileshp56@users.noreply.github.com>
2024-01-03 11:17:38 -0500

    Update pluginList.ts (#44)
    Add my plugin to your website :)

--- src/pluginList.ts
+++ src/pluginList.ts
@@ -28,6 +28,9 @@ export default [
     isFeatured: false
   },
   {
+    name: "@saileshp56/web3.js-plugin-nft-master",
+    isFeatured: false
+  },
     name: "@rudra-xyz/web3-plugin-multicall",
     isFeatured: false
   },
