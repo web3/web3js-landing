e2eedf2fa2a6559ff471a294425dc651060e87c1
Marin Petrunic <marin.petrunic@gmail.com>
2024-07-08 14:10:34 +0200

    fix: nextjs static export used so it can be deployed on cloudflare pages
    Signed-off-by: Marin Petrunic <marin.petrunic@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    distDir: 'build',
+    reactStrictMode: true,
+};
 
 export default nextConfig;
