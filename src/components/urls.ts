a8c1bae549f7346ed4d6d5f7f746afa959981c23
Marin Petrunić <mpetrunic@users.noreply.github.com>
2024-07-10 10:32:11 +0200

    update web3 github url

--- src/components/urls.ts
+++ src/components/urls.ts
@@ -3,4 +3,4 @@ export const docsUrl = "https://docs.web3js.org/"
 export const npmUrl = "https://www.npmjs.com/package/web3"
 export const blogUrl = "https://blog.chainsafe.io/tagged/web3js"
 export const blogPostv4 = "https://blog.chainsafe.io/announcing-the-full-release-of-web3-js-v4-e9e26275a3dc";
-export const githubUrl = "https://github.com/chainsafe/web3js";
+export const githubUrl = "https://github.com/web3/web3.js";
