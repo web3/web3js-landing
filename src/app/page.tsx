88339a91d0157d4d2f55be96b0e028ab23d47433
jdevcs <junaid@chainsafe.io>
2025-01-30 15:30:40 +0100

    Ambassadors update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import Ambassadors from "@/components/sections/Ambassadors";
 import Features from "@/components/sections/Features";
 import Hero from "@/components/sections/Hero";
 import LogoCloud from "@/components/sections/LogoCloud";
 import Stats from "@/components/sections/Stats";
 import Testimonials from "@/components/sections/Testimonials";
 import ThreeSteps from "@/components/sections/ThreeSteps";
-import WhyBeAmbassador from "@/components/sections/WhyBeAmbassador";
 import YouCan from "@/components/sections/YouCan";
 
 export default function Home() {
@@ -21,8 +19,6 @@ export default function Home() {
     <Stats />
     <YouCan />
     <Features />
-    <Ambassadors />
-    <WhyBeAmbassador />
     <ThreeSteps />
     <Testimonials />
     <Footer />
