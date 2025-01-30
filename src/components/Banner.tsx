a146e80ddc7dd9515a1382362820745d8ff07e55
jdevcs <junaid@chainsafe.io>
2025-01-30 15:22:36 +0100

    top banner update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,12 @@
-import { docsUrl } from "./urls"
+
 
 export default function Banner() {
   return (
     <div className="relative isolate flex justify-center items-center gap-x-6 overflow-hidden bg-brand-400 px-6 py-2 sm:px-3">
       <p className="text-md text-center font-semibold leading-6 text-gray-900">
-        v4 is here! {" "}
-        <a href={docsUrl} target="_blank" rel="noreferrer" className="whitespace-nowrap font-semibold">
-          Read all about it <span aria-hidden="true">&rarr;</span>
+      Web3.js libraries are being sunset on March 4th, 2025. For migration guides and more details please refer to
+        <a href="https://blog.chainsafe.io/web3-js-sunset/" target="_blank" rel="noreferrer" className="whitespace-nowrap font-semibold">
+        Chainsafe blog <span aria-hidden="true">&rarr;</span>
         </a>
       </p>
     </div>
