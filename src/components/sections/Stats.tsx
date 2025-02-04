f64e2ed3c0560f7b114f5e07b4c4e2e40cbe537b
Junaid <86780488+jdevcs@users.noreply.github.com>
2025-02-04 11:39:37 +0100

    updates (#86)

--- src/components/sections/Stats.tsx
+++ src/components/sections/Stats.tsx
@@ -1,27 +1,4 @@
 import SectionHeading from "../SectionHeading"
-
-const stats = [
-    {
-        name: 'Monthly npm usage',
-        value: '2M',
-    },
-    {
-        name: 'Monthly CDN requests',
-        value: '2M',
-    },
-    {
-        name: 'Weekly downloads',
-        value: '500k',
-    },
-    {
-        name: 'GitHub stars',
-        value: '19K',
-    },
-    {
-        name: 'Dependent projects',
-        value: '3K+',
-    },
-  ]
   
   export default function Stats() {
     return (
@@ -34,14 +11,7 @@ const stats = [
               Web3.js was established in 2014, making it the oldest web3 library. With extensive documentation, an active community and modular design, Web3.js is powerful and easy-to-use. 
               </p>
             </div>
-            <dl className="mt-16 grid grid-cols-1 gap-0.5 text-center sm:grid-cols-2 lg:grid-cols-5 lg:gap-x-2">
-              {stats.map((stat, index) => (
-                <div key={index} className="flex flex-col border-zinc-700 border py-4 rounded-xl">
-                  <p className="my-4 text-6xl font-semibold text-brand-200">{stat.value}</p>
-                  <p className="text-md leading-6 text-gray-400">{stat.name}</p>
-                </div>
-              ))}
-            </dl>
+
           </div>
         </div>
       </div>
